32eb170843e45c0217008a0cf0c9df296ed0edc7
Vitor Rangel <vitorfdrangel@gmail.com>
2025-04-13 18:56:38 -0300

    feat: add badge transaction type

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Transaction, TransactionType } from "@prisma/client";
+import { Badge } from "@/app/_components/ui/badge";
+import { CircleIcon } from "lucide-react";
+
+export const TransactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return (
+          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            <p>Depósito</p>
+          </Badge>
+        );
+      }
+
+      if (transaction.type === TransactionType.EXPENSE) {
+        return (
+          <Badge className="bg-danger/10 font-bold text-danger hover:bg-danger/10">
+            <CircleIcon className="mr-2 fill-danger" size={10} />
+            <p>Despesa</p>
+          </Badge>
+        );
+      }
+
+      return (
+        <Badge className="bg-white/10 font-bold text-white hover:bg-white/10">
+          <CircleIcon className="mr-2 fill-white" size={10} />
+          <p>Investimento</p>
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Forma de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
