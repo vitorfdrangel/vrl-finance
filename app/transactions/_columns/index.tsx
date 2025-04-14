2ecb248738e3028cf2a133d5a3e415684b341170
Vitor Rangel <vitorfdrangel@gmail.com>
2025-04-14 12:02:36 -0300

    feat: add payment and date formatting

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -5,6 +5,28 @@ import { Transaction } from "@prisma/client";
 
 import { TransactionTypeBadge } from "../_components/type-badge";
 
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  PIX: "Pix",
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto",
+  OTHER: "Outros",
+};
+
 export const TransactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "name",
@@ -20,14 +42,27 @@ export const TransactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Forma de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) => {
+      const date = new Date(transaction.date).toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+      });
+
+      return date;
+    },
   },
   {
     accessorKey: "amount",
