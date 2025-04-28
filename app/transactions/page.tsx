6b4fcceb84e1cef98a87252aee0aac44641f489c
Vitor Rangel <vitorfdrangel@gmail.com>
2025-04-28 00:00:53 -0300

    refactor: ensure the transaction page scrolls correctly

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,7 @@ import UpsertTransactionButton from "../_components/add-transaction-button";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -21,15 +22,17 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6 pb-4">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <UpsertTransactionButton />
         </div>
-        <DataTable
-          columns={TransactionColumns}
-          data={JSON.parse(JSON.stringify(transactions))}
-        />
+        <ScrollArea className="h-[730px] rounded-md border">
+          <DataTable
+            columns={TransactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   );
