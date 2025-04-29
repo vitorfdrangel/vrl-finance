b5a19d5e61b343ff2ba42773e76a100385f80138
Vitor Rangel <vitorfdrangel@gmail.com>
2025-04-28 23:38:08 -0300

    feat: creating signature card

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
+import { Card, CardContent, CardHeader } from "../_components/ui/card";
+import { CheckIcon, XIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -8,7 +11,58 @@ const SubscriptionPage = async () => {
     redirect("/login");
   }
 
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
+
+        <div className="flex gap-6">
+          <Card className="w-[450px]">
+            <CardHeader className="space-y-6 border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Básico
+              </h2>
+
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0,00</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+
+            <CardContent className="space-y-8 py-10">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>
+                  Apenas 10 transações por dia{" "}
+                  <span className="font-bold text-primary">7</span>/10
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <XIcon />
+                <p>Relatórios de IA ilimitados</p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <XIcon />
+                <p>....</p>
+              </div>
+
+              <Button
+                variant={"outline"}
+                className="w-full rounded-full border-primary text-primary"
+              >
+                Assinar o Plano
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SubscriptionPage;
