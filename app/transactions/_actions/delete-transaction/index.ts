73ec2f07eeb8d7529e0eb47336af6df348b9c740
Vitor Rangel <vitorfdrangel@gmail.com>
2025-05-21 18:10:28 -0300

    feat: ensure transactions can be deleted

--- app/transactions/_actions/delete-transaction/index.ts
+++ app/transactions/_actions/delete-transaction/index.ts
@@ -2,11 +2,14 @@
 
 import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
+import { DeleteTransactionSchema } from "./schema";
 
-export const deleteTransaction = async (id: string) => {
+export const deleteTransaction = async ({
+  transactionId,
+}: DeleteTransactionSchema) => {
   await db.transaction.delete({
     where: {
-      id: id,
+      id: transactionId,
     },
   });
 
--- app/transactions/_components/delete-transaction-button.tsx
+++ app/transactions/_components/delete-transaction-button.tsx
@@ -3,22 +3,67 @@
 import { Button } from "@/app/_components/ui/button";
 import { TrashIcon } from "lucide-react";
 import { deleteTransaction } from "../_actions/delete-transaction";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
+import { deleteTransactionSchema } from "../_actions/delete-transaction/schema";
+import { toast } from "sonner";
 
 interface DeleteTransactionProps {
   transactionId: string;
 }
 
 const DeleteTransactionButton = ({ transactionId }: DeleteTransactionProps) => {
+  deleteTransactionSchema.parse({ transactionId });
+
+  const handleConfirmDelete = async () => {
+    try {
+      await deleteTransaction({ transactionId });
+
+      toast.success("Transação deletada com sucesso!");
+    } catch (error) {
+      console.log(error);
+
+      toast.error("Erro ao deletar transação!");
+    }
+  };
+
   return (
-    <>
-      <Button
-        variant={"ghost"}
-        size={"icon"}
-        onClick={() => deleteTransaction(transactionId)}
-      >
-        <TrashIcon className="text-muted-foreground" />
-      </Button>
-    </>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant={"ghost"} size={"icon"}>
+          <TrashIcon className="text-muted-foreground" />
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Tem certeza que deseja deletar essa transação?
+          </AlertDialogTitle>
+
+          <AlertDialogDescription>
+            Essa ação não poderá ser desfeita.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+
+          <AlertDialogAction onClick={handleConfirmDelete}>
+            Deletar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
